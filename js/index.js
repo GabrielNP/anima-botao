bd8616da45db7c822991280610e0b3b3139fb939
gabriel.novaes <gabriel.novaes@fs.com.br>
2019-12-17 19:38:14 -0300

    iniciando prolongamento do chatbot icon por mouseover e por setInterval

--- js/index.js
+++ js/index.js
@@ -1,17 +1,48 @@
 var mainIcon = $('#chat-icon');
+var cont = 0;
 
 mainIcon.ready(function() {
     setInterval(function() {
          animateIcon();
+         if (cont == 5) {
+            cont = 0;
+            botaQuadrado();
+            console.log("bati");
+        }        
+        cont++;
     }, 6000);
 });
 
-function animateIcon() {
+function animateIcon() { console.log(cont);
     let icon  = (mainIcon.attr('src') == "img/hand.png")?
                         "img/icon-oi-avatar-02.png" :
                         "img/hand.png";
     
+        if (icon == "img/hand.png" && cont == 1) {
+            mainIcon.addClass("rotate-vert-center");
+        } 
+
+        if (icon == "img/icon-oi-avatar-02.png" && cont == 4) {
+                cont--;
+            }
+        tiraQuadrado();              
         mainIcon.toggleClass("rotate-vert-center")
             .attr("src", icon)
             .toggleClass("shake-lr btn-floating btn-large waves-effect waves-light white");
+        
+}
+
+
+mainIcon.mouseover(function() {
+    console.log("PASSEI");
+    botaQuadrado();
+});
+
+function botaQuadrado() {
+    mainIcon.removeClass("rotate-vert-center");
+    mainIcon.addClass("quadrado");
+}
+
+function tiraQuadrado() {
+    mainIcon.removeClass("quadrado"); 
 }
